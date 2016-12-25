f365927c520ef71010e426e36012a1950a7d97a1
Raúl López <rldona@gmail.com>
2016-12-25 14:17:53 +0100

    init nam project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crontabConverter = require('crontab-converter');
+
+const crontabManager = require('./lib');
+const db = require('./projects-mock');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+let cm = new crontabManager(db.projects());
+
+
+console.log('Serer start at port 3000');
+
+app.listen(3000);
